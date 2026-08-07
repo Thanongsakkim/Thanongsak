366ed8b6fd9eddf04a0987500b6dee549b48ed23
Thanongsak Srisuk <thanongsak.sr@ku.th>
2026-08-07 16:06:43 +0700

    Week_06

---
+++ labs/week06-lab/example/geometry.py
@@ -0,0 +1,12 @@
+def calculate_triangle_area(hight, base):
+    """Calculates and displays rectangle area"""
+    area = 0.5 * hight * base
+    print(f"Rectangle with hight {hight} and base {base}")
+    print(f"Area = {hight} × {base} = {area}")
+    print()
+
+print("Calculating Triangle areas:")
+calculate_triangle_area(5, 3)
+calculate_triangle_area(10, 7)
+
+# คำนวนสามเหลี่ยม
